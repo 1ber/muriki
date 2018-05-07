c345a5f9b382cc4aa0abc9062b9d957a46c29244
1ber <36287145+1ber@users.noreply.github.com>
2018-05-07 10:39:23 -0300

    Create __init__.py

---
+++ muriki/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+########################################################################
+# Muriki
+# Copyright (C) 2018, Humberto Ramos Costa
+# License: Apache Software License 2.0, see the LICENSE file
+########################################################################
+
+from .DataProperty import DataProperty
+from .DataProperty import generate_error_message
+from .DataProperty import data_property
+from .DataProperty import auto_data_property
+
+from .DataEntity import data_entity
+
